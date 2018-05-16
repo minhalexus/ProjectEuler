bf5a6e25440e942ea3c9d5da23b9f16abcae1c72
wingezda <wingezda@example.com>
2018-05-15 23:09:29 -0400

    added a slightly improved solution. Should be about x2 faster.

--- Problem 1.py
+++ Problem 1.py
@@ -1,6 +1,9 @@
 sum = 0
-for i in range (1000):
-    if (i%5 == 0) or (i%3 == 0):
+for i in range (0,1000,5):
+    sum += i
+
+for i in range(0,1000,3):
+    if (i % 5 != 0):
         sum += i
 
 print(sum)
